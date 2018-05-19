bfa7fef53729abc34fff887ab987a4cdce2b0ff6
jd <jean.daniel.michaud@gmail.com>
2018-05-19 22:44:26 +0200

    Replace all

--- src/math.js
+++ src/math.js
@@ -1,7 +1,7 @@
 import katex from 'katex';
 
 export default function math(displayMode, fragment) {
-  const unescapedFragment = fragment.replace('&amp;', '&');
+  const unescapedFragment = fragment.replace(/&amp;/g, '&');
   return Promise.resolve(katex.renderToString(unescapedFragment, {
     displayMode,
   }));
