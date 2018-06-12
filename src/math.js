f4e1d88283d743e12d559e8c82d558497333e85b
jd <jean.daniel.michaud@gmail.com>
2018-06-12 19:44:40 +0200

    Fix less and greater then sign

--- src/math.js
+++ src/math.js
@@ -1,7 +1,10 @@
 import katex from 'katex';
 
 export default function math(displayMode, fragment) {
-  const unescapedFragment = fragment.replace(/&amp;/g, '&');
+  const unescapedFragment = fragment
+    .replace(/&amp;/g, '&')
+    .replace(/&gt;/g, '>')
+    .replace(/&lt;/g, '<');
   return Promise.resolve(katex.renderToString(unescapedFragment, {
     displayMode,
   }));
